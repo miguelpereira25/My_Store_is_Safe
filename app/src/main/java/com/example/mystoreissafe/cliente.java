980181762b46ea999db106de442f37a51f28fb8e
miguelpereira25 <mig_santop@hotmail.com>
2020-06-26 15:33:43 +0100

    Alterado nos scrollView de constraint layout para linear layout Parece ter melhorado a questão de espaçamento fixo entre botoes e caixas de texto

--- app/src/main/java/com/example/mystoreissafe/cliente.java
+++ app/src/main/java/com/example/mystoreissafe/cliente.java
@@ -2,7 +2,11 @@ package com.example.mystoreissafe;
 
 import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.database.Cursor;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 
@@ -10,6 +14,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toolbar;
 import android.view.Menu;
@@ -22,7 +27,17 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class cliente extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    
+
+    private EditText editText_Name;
+    private EditText editText_contribuinte;
+    private EditText editText_contact;
+    private EditText editText_adress;
+    private EditText editText_age;
+    private EditText editText_temp;
+    private Button button_save;
+    private Button button_back;
+
+
 
 
 
@@ -32,9 +47,37 @@ public class cliente extends AppCompatActivity implements LoaderManager.LoaderCa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
         /*Toolbar toolbar = findViewById((R.id.toolbar));*/
-        setSupportActionBar();
+        //setSupportActionBar();
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        button_back = (Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmain();
+                Toast.makeText(cliente.this,getString(R.string.canceled),Toast.LENGTH_LONG).show();
+            }
+        });
 
+
+    }
+    public void openmain(){
+        Intent intent = new Intent(this,SegundoMain.class);
+        startActivity(intent);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
 
     }
 }
