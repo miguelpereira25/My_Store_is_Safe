53b8702f509610d30cbdf114330f4ee6c97fb17b
miguelpereira25 <mig_santop@hotmail.com>
2020-06-26 10:41:35 +0100

    Most of the activities are working, missing the open helper and database

--- app/src/main/java/com/example/mystoreissafe/Agenda.java
+++ app/src/main/java/com/example/mystoreissafe/Agenda.java
@@ -2,13 +2,42 @@ package com.example.mystoreissafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Agenda extends AppCompatActivity {
+    private Button button_agendar;
+    private Button button_cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+        button_agendar = (Button) findViewById(R.id.button_agendar);
+        button_agendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmain();
+                Toast.makeText(Agenda.this,getString(R.string.agendado),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button_cancelar = (Button) findViewById(R.id.button_cancelar);
+        button_cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmain();
+                Toast.makeText(Agenda.this,getString(R.string.canceled), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+    public void openmain(){
+        Intent intent = new Intent(this,SegundoMain.class);
+        startActivity(intent);
     }
 }
