4547ce73bb04e37b612c570c6bd5351f37117381
miguelpereira25 <mig_santop@hotmail.com>
2020-06-26 11:42:58 +0100

    As traduçoes estão feitas

---
+++ app/src/main/java/com/example/mystoreissafe/BOOk_apointment.java
@@ -0,0 +1,14 @@
+package com.example.mystoreissafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BOOk_apointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_o_ok_apointment);
+    }
+}
