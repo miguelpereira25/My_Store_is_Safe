3897a10c62f705359245a93eec29dada33666506
miguelpereira25 <mig_santop@hotmail.com>
2020-06-30 16:18:50 +0100

    created new background for app info activity and new settings activity.

---
+++ app/src/main/java/com/example/mystoreissafe/App_Info.java
@@ -0,0 +1,14 @@
+package com.example.mystoreissafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class App_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app__info);
+    }
+}
